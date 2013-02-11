f4a9c1194742547a4397606165b8f36951038e3c
singh_dd93 <singh_dd93@yahoo.in>
2013-02-12 00:52:47 +0530

    Fixed Dependencies & Build with linaro Some Boot Fixes
    Change-Id: Ib5802c435e8a2cdfda3122c27ade46e6c7224368

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,8 +2,8 @@
 -include vendor/semc/haida/BoardConfigVendor.mk
 
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
-TARGET_KERNEL_CONFIG := cyanogen_haida_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+TARGET_KERNEL_CONFIG := pa_haida_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7.3
 
 TARGET_RECOVERY_INITRC := device/semc/haida/recovery/init.rc
 
